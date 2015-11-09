b633e89adbca4123db3ce690643105b9898a9f45
Bennet Bartmann <bennet.bartmann@googlemail.com>
2015-11-09 21:30:09 +0100

    Update PingObservable.py

--- Communication/PingObservable.py
+++ Communication/PingObservable.py
@@ -3,11 +3,8 @@ from Communication.Observable import Observable
 
 class PingObservable(Observable):
 
-    def input(self, data):
-        self._data = data
-        # hier kann noch gecheckt werden, ob data wirklich ein server ist, der ping haben will, oder sonstwas
-        # finde heraus, wer zurückgepingt werden muss, und ob das überhaupt ein ping-request ist oder ein user sich
-        # einen spass erlaubt hat
-        self._data = data
-        self.notifyObservers()
+    def input(self, rawline):
+
+        #TODO: Please split the data in data['raw'] data['source'] 
+        self.notifyObservers(data)
         print(data)
