7c27b37196bef05a9d2d7563f06eb2e4c8ce19a4
Bennet Bartmann <bennet.bartmann@googlemail.com>
2015-11-24 19:09:58 +0100

    Update Connection.py

--- Communication/Connection.py
+++ Communication/Connection.py
@@ -30,7 +30,7 @@ class Connection(object):
         """
         receive from Network
         """
-        data = self.irc.recv(4096).decode('UTF-8', errors='backslashreplace')
+        data = self.irc.recv(4096) #.decode('UTF-8', errors='backslashreplace')
         data = data.rstrip()
         print(data)
         if data.find('PING') != -1:
