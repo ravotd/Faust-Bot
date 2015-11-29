7facc4b9faade5d2ec5c42d6901f8369ae6f42ac
ravotd <daniela.i.tovar@gmail.com>
2015-11-30 00:26:27 +0100

    first try on loading config from file

--- Model/ConnectionDetails.py
+++ Model/ConnectionDetails.py
@@ -1,24 +1,38 @@
 class ConnectionDateils(object):
+
     def get_server(self):
         """
         :return: the server to connect to
         """
-        return "irc.freenode.org"
+        return self.data['server']
 
     def get_nick(self):
         """
         :return: own nick
         """
-        return "FaustBot"
+        return self.data['nick']
 
     def get_channel(self):
         """
         :return: the channel connected into
         """
-        return "#faust-bot"
+        return self.data['channel']
 
     def get_port(self):
-        return 6667
+        return int(self.data['port'])
 
     def get_lang(self):
-        return "de-de"
+        return self.data['lang']
+
+    def change_lang(self, lang):
+        self.data['lang'] = lang
+
+    def __init__(self):
+        txt = open('config.txt')
+        self.data = {}
+        data = txt.readline()
+        while (data != ''):
+            # TODO ERROR next line won't work with /r/n as line end in config file! that has to be fixed before release!
+            self.data[data.split(': ')[0]] = data.split(': ')[1][0:-1]
+            data = txt.readline()
+        print(self.data)
