81d8fb45e43f322751aaf0d17b9ad67448b50364
ravotd <daniela.i.tovar@gmail.com>
2015-12-02 23:55:33 +0100

    title tuts meistens, tweets sind noch unschön

--- Controler/TitleObserver.py
+++ Controler/TitleObserver.py
@@ -6,6 +6,15 @@ from Communication.Connection import Connection
 from Controler.PrivMsgObserverPrototype import PrivMsgObserverPrototype
 
 
+def beautify_title(title):
+    """
+    main purpose: make tweets nicer
+    :param title: string to beautify
+
+    """
+    return title
+
+
 class TitleObserver(PrivMsgObserverPrototype):
 
     def update_on_priv_msg(self, data):
@@ -16,10 +25,17 @@ class TitleObserver(PrivMsgObserverPrototype):
                 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
                 url = url.string
                 req = urllib.request.Request(url, None, headers)
-                content = urllib.request.urlopen(req)
-                print(content)
+                resource = urllib.request.urlopen(req)
+                encoding = resource.headers.get_content_charset()
+                # der erste Fall kann raus, wenn ein anderer Channel benutzt wird
+                if url.find('rehakids.de') != -1:
+                    encoding = 'windows-1252'
+                if not encoding:
+                    encoding = 'utf-8'
+                content =  resource.read().decode(encoding)
                 titleRE = re.compile("<title>(.+?)</title>")
-                title = titleRE.search(content.read().decode('utf8')).group(1)
+                title = titleRE.search(content).group(1)
+                title = beautify_title(title)
                 print(title)
                 Connection.singleton().send_channel(title)
             except Exception as exc:
