79567c7ec2ab72e5b7a02870d5ec0d5321002ed8
Daniela Tovar <starly@vsrv23885.customer.xenway.de>
2015-12-02 22:15:33 +0000

    fixed title - still only for nice servers

--- Controler/TitleObserver.py
+++ Controler/TitleObserver.py
@@ -1,5 +1,6 @@
 import re
 import urllib
+from urllib import request
 
 from Communication.Connection import Connection
 from Controler.PrivMsgObserverPrototype import PrivMsgObserverPrototype
@@ -10,11 +11,16 @@ class TitleObserver(PrivMsgObserverPrototype):
     def update_on_priv_msg(self, data):
         url = re.search("(?P<url>https?://[^\s]+)", data['message'])
         if url is not None:
+            print(url)
             try:
                 url = url.string
+                print(url)
                 content = urllib.request.urlopen(url)
+                print(content)
                 titleRE = re.compile("<title>(.+?)</title>")
                 title = titleRE.search(content.read().decode('utf8')).group(1)
+                print(title)
                 Connection.singleton().send_channel(title)
             except Exception as exc:
+                print(exc)
                 pass
