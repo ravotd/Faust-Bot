22fbb5373ca11eb905e4bdb6a9a8073afd9f787b
ravotd <daniela.i.tovar@gmail.com>
2015-12-05 01:35:02 +0100

    -

--- Main.py
+++ Main.py
@@ -19,10 +19,7 @@ def setup():
     # while names-module wasn't called yet
     #     receive()
     Connection.singleton().receive()
-    data = Connection.singleton().last_data()
-    data = data.split(' ')
-    data = data[1]
-    while data != '353':
+    while Connection.singleton().last_data().split(' ')[1] != '353':
         Connection.singleton().receive()
 
     Connection.singleton().observePing(PingAnswerObserver.ModulePing())
