a25917023fa70d8aeec661834a96f72e520c2004
Bennet Bartmann <bennet.bartmann@googlemail.com>
2015-12-06 04:01:00 +0100

    fixed wiki module reacting to wikipedia links

--- Controler/WikiObserver.py
+++ Controler/WikiObserver.py
@@ -14,7 +14,7 @@ class WikiObserver(PrivMsgObserverPrototype):
         q = data['message'].split(' ')
         query = ''
         for word in q:
-            if word != '.w':
+            if word != '.w ':
                 query += word + ' '
         w = wikipedia.search(query)
         if w.__len__() == 0:
