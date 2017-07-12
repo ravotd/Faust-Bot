c11a83afd5d5a065261910a4ad73fb845c07eba7
Kristoffer Schneider <kschneider@codingfalx.de>
2017-07-12 22:34:14 +0200

    Fixed LoveAndPeace Message

--- FaustBot/Modules/LoveAndPeaceObserver.py
+++ FaustBot/Modules/LoveAndPeaceObserver.py
@@ -6,5 +6,5 @@ class LoveAndPeaceObserver(PrivMsgObserverPrototype):
     def update_on_priv_msg(self, data: dict, connection: Connection):
         if data['message'].find('.peace') == -1:
             return
-        connection.send_back('\001ACTION hüpft durch den raum und schmeißt blumen um sich und singt: \"Love and '
-                             'Peace, wir haben uns alle lieb!..\".\001', data)
+        connection.send_back('\001ACTION hüpft durch den Raum, schmeißt Blumen um sich und singt: \"Love and '
+                             'Peace, wir haben uns alle lieb..!\".\001', data)
