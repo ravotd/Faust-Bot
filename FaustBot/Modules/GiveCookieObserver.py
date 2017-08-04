91f1e5a4c42bac144512ed9b4e66237ecce91598
Sophie Bartmann <sophie@bartmann.email>
2017-08-04 20:43:25 +0200

    New Cookies:)

--- FaustBot/Modules/GiveCookieObserver.py
+++ FaustBot/Modules/GiveCookieObserver.py
@@ -1,9 +1,13 @@
 from FaustBot.Communication import Connection
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
+import random
 
+kekse = ['einen Schokoladenkeks', 'einen Vanillekeks', 'einen Doppelkeks', 'keinen Keks',
+         'einen Keks','einen Erdbeerkeks','einen Schokoladen Cheescake keks']
 
 class GiveCookieObserver(PrivMsgObserverPrototype):
+
     def update_on_priv_msg(self, data: dict, connection: Connection):
         if data['message'].find('.cookie') == -1:
             return
-        connection.send_back('\001ACTION schenkt ' + data['nick'] + ' einen Keks.\001', data)
+        connection.send_back('\001ACTION schenkt ' + data['nick'] + ' '+random.choice(kekse)+'.\001', data)
