b1c58a5d41e853c7ea8878b804795b5735c0c6a3
Sophie Bartmann <bennet.bartmann@gmail.com>
2018-02-10 18:26:43 +0100

    slight improvments

--- essen.py
+++ essen.py
@@ -1,4 +1,5 @@
 
 essen = ['einen Doener', 'einen Wirsingeintopf', 'einen Lahmacun', 'eine Portion Kartoffelbrei',
                      'Spinat', 'Astronautenfutter', 'einen Snickers', 'einen Mars', 'eine Brezel',
-                     'eine Portion Gagh', 'Filet Mignon']
+                     'eine Portion Gagh', 'Filet Mignon', 'ein Döner', 'ein Teller Sphagetti Blogonaise',
+                     'ein Teller Sphagetti Käsesahnesoße']
