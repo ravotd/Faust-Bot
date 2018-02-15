697fe90a11861b8648628094baf6eec810e01d65
Solotalent <36490644+Solotalent84@users.noreply.github.com>
2018-02-15 10:55:43 +0100

    Update essen.py (#68)
    * Update essen.py
    
    * Update essen.py

--- essen.py
+++ essen.py
@@ -1,5 +1,17 @@
 
-essen = ['einen Doener', 'einen Wirsingeintopf', 'einen Lahmacun', 'eine Portion Kartoffelbrei',
-                     'Spinat', 'Astronautenfutter', 'einen Snickers', 'einen Mars', 'eine Brezel',
-                     'eine Portion Gagh', 'Filet Mignon', 'ein Döner', 'ein Teller Sphagetti Blogonaise',
-                     'ein Teller Sphagetti Käsesahnesoße']
+essen = ['einen Wirsingeintopf', 'einen Lahmacun', 'eine Portion Kartoffelbrei',
+                     'Spinat', 'Astronautenfutter', 'ein Snickers', 'einen Mars-Riegel', 'eine Brezel',
+                     'eine Portion Gagh', 'Filet Mignon', 'einen Döner', 'ein Teller Spaghetti Bolognese',
+                     'ein Teller Spaghetti mit Käsesahnesoße', 'einen Obstteller', 'eine Hand voll Gummibärchen',
+                     'ein gummiartiges Hühnchen', 'eine Schale mit Kartoffelchips', 'zwei Käsebrote',
+                     'Okonomiyaki', 'zwei Stück Erdbeertorte', 'feuriges Chili con Carne', 'ein fluffiges Rührei',
+                     'eine leckere Pizza', 'einen frischen Gurkensalat', 'einen großen Chefsalat',
+                     'eine übelriechende Käsesuppe', 'Fischstäbchen und Kartoffelbrei', 'ungarisches Gulasch',
+                     'Spaghetti mit Soja-Bolognese', 'ein original französisches Ratatouille', 'einen sahnigen Schokopudding',
+                     'arme Ritter', 'einen Festtagsbraten mit Klößen', 'Pommes mit Majo', 'Corvidaes leckeren Kartoffelauflauf',
+                     'Krabben-Cocktail', 'einen Toast Hawaii', 'indisches Lamm Curry', 'einen Gyrosteller mit Tsatsiki',
+                     'ein Pilz-Risotto', 'ein hart gekochtes Ei', 'vier Pralinen mit Nougatfüllung', 'eine kalte Bockwurst',
+                     'ein paar Mini-Frühlingsrollen', 'einen Glückskeks mit dem Spruch \"Du wirst heute nicht satt.\" ',
+                     'würziges Kassler mit Sauerkraut', 'ein Schälchen Apfelmus', 'Bratkartoffeln', 'asiatisches Wokgemüse',
+                     'einen fruchtigen Lolli', 'einen Teller mit süßem Milchreis', 'eine Lasagne', 'chinesisch gebratene Nudeln',
+                     'ein Thunfisch-Baguette', 'fünf mexicanische Tacos', 'einen Blaubeer-Muffin mit Sahne', 'eine Hand voll alte Erdnüsse']
