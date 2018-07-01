f13f828c110e29c8a163a8bc1f7d023c4d66d2a8
Kristoffer Schneider <kschneider@codingfalx.de>
2018-07-01 22:46:37 +0200

    Faust-Bot-97 Refactor Observer - refactored UserList to work with multi channel support
    Signed-off-by: Kristoffer Schneider <kschneider@codingfalx.de>

--- FaustBot/Modules/UserList.py
+++ FaustBot/Modules/UserList.py
@@ -1,3 +1,7 @@
+from typing import Union
+
+from FaustBot.Communication.Connection import Connection
+from FaustBot.Model.IRCData import IRCData
 from FaustBot.Model.RemoteUser import RemoteUser
 from FaustBot.Modules.JoinObserverPrototype import JoinObserverPrototype
 from FaustBot.Modules.ModuleType import ModuleType
@@ -23,34 +27,37 @@ class UserList(JoinObserverPrototype, KickObserverPrototype, LeaveObserverProtot
     def get_module_types():
         return [ModuleType.ON_JOIN, ModuleType.ON_KICK, ModuleType.ON_LEAVE, ModuleType.ON_NICK_CHANGE]
 
-    def update_on_kick(self, data, connection):
-        if data['nick'] in self.userList:
-            del self.userList[data['nick']]
-#         print(self.userList)
+    def update_on_kick(self, data: IRCData, connection: Connection):
+        self._remove_user(data.nick, data.channel)
 
-    def update_on_leave(self, data, connection):
-        if data['nick'] in self.userList:
-            del self.userList[data['nick']]
-#         print(self.userList)
+    def update_on_leave(self, data: IRCData, connection: Connection):
+        self._remove_user(data.nick, data.channel)
 
     def update_on_join(self, data, connection):
-        self.userList[data['nick']] = RemoteUser(data['nick'], data['user'], data['host'])
-#         print(self.userList)
+        self._add_user(data)
 
-    def update_on_nick_change(self, data, connection):
-        if data['old_nick'] in self.userList:
-            remuser = self.userList[data['old_nick']]
-            del self.userList[data['old_nick']]
-        else:
-            # shouldn't happen but let's be safe.
-            remuser = RemoteUser('UN.KNOWN', 'UN.KNOWN', 'UN.KNOWN')
-
-        remuser.nick = data['new_nick']
-        self.userList[data['new_nick']] = remuser
-#         print(self.userList)
+    def update_on_nick_change(self, data: IRCData, connection: Connection):
+        channel_userlist = self.userList[data.channel]
+        user = self._remove_user(data.nick, data.channel)
+        user.nick = data.message
+        channel_userlist[user.nick] = user
 
     def clear_list(self):
         self.userList = {}
 
-    def add_user(self, remuser):
-        self.userList[remuser.nick] = remuser
+    def _add_user(self, data: IRCData):
+        user = RemoteUser(data.nick, data.user, data.host)
+        channel_userlist = self.userList[data.channel]
+        if channel_userlist is None:
+            channel_userlist = {}
+            self.userList[data.channel] = channel_userlist
+        channel_userlist[data.nick] = user
+
+    def _remove_user(self, nick: str, channel: str) -> Union[RemoteUser, None]:
+        channel_userlist = self.userList[channel]
+        if channel_userlist is not None:
+            if nick in channel_userlist:
+                user = channel_userlist[nick]
+                del channel_userlist[nick]
+                return user
+        return None
