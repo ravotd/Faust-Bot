54cfc26bad32e1072e4a5874b2787d71bebe3fad
Kristoffer Schneider <kschneider@codingfalx.de>
2018-07-06 17:25:45 +0200

    Faust-Bot-97 Refactor Observer - ComicObserver
    Signed-off-by: Kristoffer Schneider <kschneider@codingfalx.de>

--- FaustBot/Modules/ComicObserver.py
+++ FaustBot/Modules/ComicObserver.py
@@ -1,13 +1,10 @@
 import random
 import urllib
-import requests
-import html
 
 from FaustBot.Communication.Connection import Connection
+from FaustBot.Modules.ComicScraper import ComicScraper
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
 from FaustBot.Modules.TitleObserver import TitleObserver
-from FaustBot.Modules.ComicScraper import ComicScraper
-
 from comics import *
 
 
@@ -20,24 +17,24 @@ class ComicObserver(PrivMsgObserverPrototype):
     def help():
         return '.comic liefer einen Link zu einem zufälligen Comic.'
 
-    def update_on_priv_msg(self, data: dict, connection: Connection):
-        if data['message'].find('.comic') == -1:
+    def update_on_priv_msg(self, data, connection: Connection):
+        if data.message.find('.comic') == -1:
             return
-            
-        #Join list of comics that have a web based random functionality and those that need a scraper
-        all_comics=comics+scraper_comics
-        
-        #Choose from the joined list
+
+        # Join list of comics that have a web based random functionality and those that need a scraper
+        all_comics = comics + scraper_comics
+
+        # Choose from the joined list
         comic = random.choice(all_comics)
-        
-        #Check which type of comic it is: If it's one that doesn't need a scaper, get the url and return it.
-        #If it needs a scraper, use ComicScraper to scrape the comic.
-        #If you want to add custom comic scrapers: Look at ComicScraper.py and insert your functionality.
-        if not comic in scraper_comics:
+
+        # Check which type of comic it is: If it's one that doesn't need a scaper, get the url and return it.
+        # If it needs a scraper, use ComicScraper to scrape the comic.
+        # If you want to add custom comic scrapers: Look at ComicScraper.py and insert your functionality.
+        if comic not in scraper_comics:
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
             req = urllib.request.Request(comic, None, headers)
             resource = urllib.request.urlopen(req)
             title = TitleObserver.getTitle(TitleObserver(), resource)
             connection.send_back(resource.geturl() + " " + title, data)
         else:
-            connection.send_back(ComicScraper.getRandomComic(comic),data);
+            connection.send_back(ComicScraper.getRandomComic(comic), data)
