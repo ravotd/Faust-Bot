f791fea18769af55e54c91c297e445c2d85ced75
Sophie Bartmann <sophie.e.bartmann@gmail.com>
2019-01-31 09:03:10 +0100

    Added naive Score functionality

--- FaustBot/Modules/HangmanObserver.py
+++ FaustBot/Modules/HangmanObserver.py
@@ -1,7 +1,7 @@
 
 from FaustBot.Communication.Connection import Connection
 from FaustBot.Modules.PrivMsgObserverPrototype import PrivMsgObserverPrototype
-
+from collections import defaultdict
 
 class HangmanObserver(PrivMsgObserverPrototype):
     @staticmethod
@@ -18,6 +18,8 @@ class HangmanObserver(PrivMsgObserverPrototype):
         self.guesses = ['-','/',' ','_']
         self.leftTrys = 0
         self.wrongGuesses = []
+        self.score = defaultdict(int)
+        self.worder = ''
 
     def update_on_priv_msg(self, data, connection: Connection):
         if data['message'].find('.guess ') != -1:
@@ -31,9 +33,14 @@ class HangmanObserver(PrivMsgObserverPrototype):
             self.guesses = []
             self.leftTrys = 0
             self.wrongGuesses =[]
+            self.worder =''
         if data['message'].find('.hint') != -1:
             self.hint(data, connection)
+        if data['message'].find('.score') != -1:
+            self.printScore(data, connection)
 
+    def printScore (self,data,connection):
+        connection.send_back(data['nick']+" hat einen Score von: "+str(self.score[data['nick']]), data)
     def hint(self,data,connection):
         wrongGuessesString = "Falsch geratene Buchstaben bis jetzt:"
         for w in self.wrongGuesses:
@@ -52,15 +59,18 @@ class HangmanObserver(PrivMsgObserverPrototype):
             connection.send_channel("Flüstere mir ein neues Wort mit .word WORT")
             return
         if tried == self.word:
+            self.score[data['nick']]+=self.countMissing()+5
             self.word = ''
             connection.send_channel("Korrekt: "+ tried)
             return
         if tried in self.word:
+            self.score[data['nick']] += 1
             self.guesses.append(tried)
         else:
             self.leftTrys -= 1
+            self.score[data['nick']] -= 1
             self.wrongGuesses.append(tried)
-        connection.send_channel(self.prepareWord())
+        connection.send_channel(self.prepareWord(data))
 
     def takeword(self, data, connection):
         if self.word == '':
@@ -73,11 +83,12 @@ class HangmanObserver(PrivMsgObserverPrototype):
             self.leftTrys = 11
             connection.send_back( "Danke für das Wort, es ist nun im Spiel!", data)
             connection.send_channel("Das Wort ist von: "+data['nick'])
-            connection.send_channel(self.prepareWord())
+            self.worder = data['nick']
+            connection.send_channel(self.prepareWord(data))
         else:
             connection.send_back("Sorry es läuft bereits ein Wort", data)
 
-    def prepareWord(self):
+    def prepareWord(self, data):
         outWord = ""
         failedChars = 0
         for char in self.word:
@@ -88,11 +99,20 @@ class HangmanObserver(PrivMsgObserverPrototype):
                 failedChars += 1
         if failedChars == 0:
             outWord = "Das ist korrekt: "+self.word
+            self.score[data['nick']] += 5
             self.word = ''
             return outWord
         if self.leftTrys == 0:
+            self.score[self.worder] += 5
             outWord = "Das richtige Wort wäre gewesen:" + self.word
             self.word = ''
             return outWord
         outWord += "Verbleibende Rateversuche: "+str(self.leftTrys)
         return outWord
+
+    def countMissing(self):
+        failedChars = 0
+        for char in self.word:
+            if char not in self.guesses:
+                failedChars += 1
+        return failedChars
