a2ae51e1e962fa6b3a2b9a1168145bff1f6f7d4d
Sophie <sophie.e.bartmann@gmail.com>
2020-09-15 10:37:39 +0200

    Minor Error Correction

--- AdaptHanWordsFromLegacy.py
+++ AdaptHanWordsFromLegacy.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 from FaustBot.Model.HanDatabaseProvider import HanDatabaseProvider
 import csv
 HanDBProvider = HanDatabaseProvider()
@@ -8,8 +11,8 @@ for word in wordListWords:
     print(word)
     print(word[1].strip())
     no = False
-    for char in  ['ä','ü','ö','ß']:
-        if char.upper() in word[1].strip().upper():
-            no = True
+    #for char in  ['ä','ü','ö','ß']:
+     #   if char.upper() in word[1].strip().upper():
+    #        no = True
     if not no:
         HanDBProvider.addWord(word[1].strip())
