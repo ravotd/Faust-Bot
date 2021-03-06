bb8ddad1741b37d53d38324e998a5f6d7e71f7e4
Sophie <sophie.e.bartmann@gmail.com>
2021-03-06 13:53:41 +0100

    Added Kraft as Extra

--- extras.py
+++ extras.py
@@ -1,3 +1,4 @@
 giveextras=['eine Rolle Toilettenpapier', 'eine Schmerztablette', 'eine Packung Girlanden',
             '...äh serviert ist das falsche Wort, gibt sollte das heißen, eine Decke.',
-            'eine Tafel Schokolade', 'eine Tüte Chips', 'eine Wärmflasche in Teddy Bezug']
+            'eine Tafel Schokolade', 'eine Tüte Chips', 'eine Wärmflasche in Teddy Bezug',
+            'ein Gartenteich voll Kraft']
