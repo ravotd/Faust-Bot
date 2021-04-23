d679a54f55871ba1e6ffc0fdea5aaa34c626d99f
Sophie Bartmann <sophie.e.bartmann@gmail.com>
2021-04-23 18:37:37 +0200

    Update extras.py
    corrected spelling mistake

--- extras.py
+++ extras.py
@@ -2,5 +2,5 @@ giveextras=['eine Rolle Toilettenpapier', 'eine Schmerztablette', 'eine Packung 
             '...äh serviert ist das falsche Wort, gibt sollte das heißen, eine Decke.',
             'eine Tafel Schokolade', 'eine Tüte Chips', 'eine Wärmflasche in Teddy Bezug',
             'ein Gartenteich voll Kraft', 'ein Fass Kraft', 'eine Tonne Kraft',
-            'ein Knuddelverrückter Kraft-Glücksbringer','ein Kubikmeter Kraft',
+            'einen knuddelverrückten Kraft-Glücksbringer','ein Kubikmeter Kraft',
             'viel Kraft... sehr viel']
